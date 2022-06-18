a8ffe1a37dff63dc9b0ec337dcf62dc533b68cf4
Mujtaba Ali <mujiroy01@gmail.com>
2022-06-18 17:27:14 +0500

    Working on dashboard...

--- src/components/sidebar-components/Dashboard.js
+++ src/components/sidebar-components/Dashboard.js
@@ -1,16 +1,36 @@
-// import React from 'react';
-// import {Paper} from '@material-ui/core';
-
-// export default function Dashboard() {
-//  <>
-// <div class="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-md-4 MuiGrid-grid-lg-3 css-6fiaes"><div class="MuiPaper-root MuiPaper-elevation MuiPaper-rounded MuiPaper-elevation1 css-t1zeje"><h2 class="MuiTypography-root MuiTypography-h6 MuiTypography-gutterBottom css-nfbrjd">Recent Deposits</h2><p class="MuiTypography-root MuiTypography-h4 css-1xvinid">$3,024.00</p><p class="MuiTypography-root MuiTypography-body1 css-1qhqt6">on 15 March, 2019</p><div><a class="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineAlways css-ley7ky" href="#">View balance</a></div></div></div>
-// re
-//  </>
-// }
-import React from 'react'
-
+import React from "react";
+import { Box, Paper, Grid } from "@material-ui/core";
+import "../../assets/css/myTailwind.css";
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
-  )
+    <>
+      <Box>
+      {/* Line 1 */}
+        <Paper className=" h-16">
+          <div className=" m-5">Heelo world</div>
+        </Paper>
+
+        {/* Line 2 */}
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <Paper>
+              <div className=" m-5">Heelo world</div>
+            </Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper>
+              <div className=" m-5">Heelo world</div>
+            </Paper>
+          </Grid>
+           
+        </Grid>
+
+
+        {/* Line 3 */}
+        <Paper>
+          <div className=" m-5">Graph</div>
+        </Paper>
+      </Box>
+    </>
+  );
 }
